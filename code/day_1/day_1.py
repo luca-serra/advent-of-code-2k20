a89fae0fec22775e2d70a7ae028c61f713ab1814
luca-serra <luca.serra@student.ecp.fr>
2020-12-02 12:56:50 +0100

    Changed repo structure

--- code/day_1/day_1.py
+++ code/day_1/day_1.py
@@ -1,23 +1,25 @@
-INPUT_FILE = "input.txt"
+INPUT_FILE = "../data/day_1.txt"
 
 input_list = [int(integer) for integer in open(INPUT_FILE).readlines()]
 
 
 # %% PART 1
+print("#======= Part 1 =======#")
 
 for i in range(len(input_list) - 1):
     for j in range(i, len(input_list)):
         if input_list[i] + input_list[j] == 2020:
-            print(input_list[i], input_list[j])
-            print(input_list[i] * input_list[j])
+            print(f"The two numbers are: {input_list[i]} and {input_list[j]}")
+            print(f"Their product is: {input_list[i] * input_list[j]}\n")
             break
 
 # %% PART 2
+print("#======= Part 2 =======#")
 
 for i in range(len(input_list) - 2):
     for j in range(i, len(input_list) - 1):
         for k in range(j, len(input_list)):
             if input_list[i] + input_list[j] + input_list[k] == 2020:
-                print(input_list[i], input_list[j], input_list[k])
-                print(input_list[i] * input_list[j] * input_list[k])
+                print(f"The three numbers are: {input_list[i]}, {input_list[j]} and {input_list[k]}")
+                print(f"Their product is: {input_list[i] * input_list[j] * input_list[k]}\n")
                 break
