bc609df9627bbe1ecc455408b1363fdb311404ae
luca-serra <luca.serra@student.ecp.fr>
2020-12-02 14:54:12 +0100

    Minor clean changes

--- code/day_1/day_1.py
+++ code/day_1/day_1.py
@@ -1,25 +1,30 @@
-INPUT_FILE = "../data/day_1.txt"
+INPUT_FILE = "../../data/day_1.txt"
 
-input_list = [int(integer) for integer in open(INPUT_FILE).readlines()]
+if __name__ == "__main__":
 
+    input_list = [int(integer) for integer in open(INPUT_FILE).readlines()]
 
-# %% PART 1
-print("#======= Part 1 =======#")
+    # PART 1
 
-for i in range(len(input_list) - 1):
-    for j in range(i, len(input_list)):
-        if input_list[i] + input_list[j] == 2020:
-            print(f"The two numbers are: {input_list[i]} and {input_list[j]}")
-            print(f"Their product is: {input_list[i] * input_list[j]}\n")
-            break
+    print("#======= Part 1 =======#")
 
-# %% PART 2
-print("#======= Part 2 =======#")
-
-for i in range(len(input_list) - 2):
-    for j in range(i, len(input_list) - 1):
-        for k in range(j, len(input_list)):
-            if input_list[i] + input_list[j] + input_list[k] == 2020:
-                print(f"The three numbers are: {input_list[i]}, {input_list[j]} and {input_list[k]}")
-                print(f"Their product is: {input_list[i] * input_list[j] * input_list[k]}\n")
+    for i in range(len(input_list) - 1):
+        for j in range(i + 1, len(input_list)):
+            if input_list[i] + input_list[j] == 2020:
+                print(f"The two numbers are: {input_list[i]} and {input_list[j]}")
+                print(f"Their product is: {input_list[i] * input_list[j]}\n")
                 break
+
+    # PART 2
+
+    print("#======= Part 2 =======#")
+
+    for i in range(len(input_list) - 2):
+        for j in range(i + 1, len(input_list) - 1):
+            for k in range(j + 1, len(input_list)):
+                if input_list[i] + input_list[j] + input_list[k] == 2020:
+                    print(
+                        f"The three numbers are: {input_list[i]}, {input_list[j]} and {input_list[k]}"
+                    )
+                    print(f"Their product is: {input_list[i] * input_list[j] * input_list[k]}\n")
+                    break
--- data/README.md
+++ data/README.md
@@ -1,13 +1,13 @@
 # Data
 
-This folder must contain the daily 'puzzles'. To execute the code of a given day *n*, please reproduce these steps:
+This folder must contain the daily 'puzzles' which are personal and provided by Advent of Code. To execute the code of a given day *n*, please reproduce these steps:
 
-1. Login for the 2020 edition on the advent of code [website](https://adventofcode.com/).
+1. Signup/signin for the 2020 edition on the advent of code [website](https://adventofcode.com/).
 
 2. Download your personal puzzle for day *n*.
 
 3. Copy/paste the given puzzle in a text file called `day_n.txt`.
 
-4. Put this text file in this folder (`data`).
+4. Move the text file into this folder.
 
 Go back to the root of the repository to see the next steps.
