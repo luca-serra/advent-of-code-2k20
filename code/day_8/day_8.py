157e2f76cb08d1583e69da58d0d67aaf0298b81b
luca-serra <luca.serra@student.ecp.fr>
2020-12-08 10:33:07 +0100

    Day 8

---
+++ code/day_8/day_8.py
@@ -0,0 +1,99 @@
+"""
+Day 8: Handheld Halting
+
+Goal: Execute a list of instructions given certain rules.
+"""
+
+INPUT_FILE = "../../data/day_8.txt"
+
+if __name__ == "__main__":
+
+    input_list = [str(string) for string in open(INPUT_FILE).readlines()]
+    input_list[-1] += "\n"
+    input_list = [string[:-1] for string in input_list]  # Deleting the '\n' character
+
+    # print(input_list)
+    instruction_indices = []
+    end_of_program = False
+    acc = 0
+    idx = 0
+    n = len(input_list)
+
+    # Part 1
+    while end_of_program is False:
+        instruction_indices.append(idx)
+        if len(set(instruction_indices)) != len(instruction_indices):
+            end_of_program = True
+            print(f"(Part 1) Instruction already executed, value of acc: {acc}")
+
+        string = input_list[idx]
+        number = int(string[5:])
+        if string[4] == "-":
+            number *= -1
+
+        if string.startswith("acc"):
+            acc += number
+            idx += 1
+        elif string.startswith("jmp"):
+            idx += number
+        else:  # starts with 'nop'
+            idx += 1
+    # print(instruction_indices)
+    indices_nop = [
+        i for i, x in enumerate(input_list) if x.startswith("nop")
+    ]  # list of indices tried to be modified to get the program to work
+    indices_jmp = [
+        i for i, x in enumerate(input_list) if x.startswith("jmp")
+    ]  # list of indices tried to be modified to get the program to work
+    indices_to_try = indices_nop + indices_jmp
+    indices_tried = []
+    proper_ending = False
+    instruction_indices = []
+    end_of_program = False
+    acc = 0
+    idx = 0
+    # print(indices_to_try)
+
+    # Part 2
+    for ind in indices_to_try:
+        if proper_ending:
+            print(f"(Part 2) Proper ending, value of acc: {acc}")
+            break
+        end_of_program = False
+        while end_of_program is False:
+            instruction_indices.append(idx)
+            if len(set(instruction_indices)) != len(instruction_indices):
+                end_of_program = True
+                acc = 0
+                idx = 0
+                instruction_indices = []
+
+            string = input_list[idx]
+            if idx == ind:
+                if idx in indices_jmp:
+                    string = "nop" + string[3:]
+                elif idx in indices_nop:
+                    string = "jmp" + string[3:]
+
+            number = int(string[5:])
+            if string[4] == "-":
+                number *= -1
+            if string.startswith("acc"):
+                acc += number
+                idx += 1
+            elif string.startswith("jmp"):
+                idx += number
+                if idx > n + 1:
+                    end_of_program = True
+                    acc = 0
+                    idx = 0
+                    instruction_indices = []
+                if idx == n + 1:
+                    end_of_program = True
+                    proper_ending = True
+            else:  # starts with 'nop'
+                idx += 1
+
+            if idx == n:
+                end_of_program = True
+                proper_ending = True
